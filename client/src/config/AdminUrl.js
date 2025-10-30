34d90d258a0c8de8995c2bfbd30d0c769b63dbf8
lakshmanbhukya <lakshmannaikbhukya17@gmail.com>
2025-10-30 20:09:47 +0530

    client deployment

--- client/src/config/AdminUrl.js
+++ client/src/config/AdminUrl.js
@@ -1,6 +1,6 @@
 export const Frontend_Admin_Url = import.meta.env.VITE_FRONTEND_ADMIN_URL || "";
 export const Backend_Root_Url =
-  import.meta.env.VITE_BACKEND_ROOT_URL || "http://127.0.0.1:5000";
+  import.meta.env.VITE_BACKEND_ROOT_URL || "https://portfolio-website-project-08tl.onrender.com";
 
 export default {
   Frontend_Admin_Url,
